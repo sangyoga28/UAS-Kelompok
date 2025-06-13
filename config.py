32eadbe97e828510a0f5c0d07b3cb73693c1608f
SeptianRafli2917 <septianraflisyahputra@gmail.com>
2025-06-13 16:59:20 +0700

    backend selesai

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    MYSQL_HOST = '127.0.0.1'
+    MYSQL_PORT = 3306
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'zakat_fitrah'
+    SECRET_KEY = ''  # Optional, for additional Flask features
